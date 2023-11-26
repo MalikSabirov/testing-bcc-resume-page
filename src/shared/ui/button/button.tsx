5a030c233b4496f0a5851e0c6b354486c1ac487f
MalikSabirov <maaliksabirov@gmail.com>
2023-11-26 17:40:14 +0600

    fix: button styles when theme changes

--- src/shared/ui/button/button.tsx
+++ src/shared/ui/button/button.tsx
@@ -10,7 +10,7 @@ export const Button: FC<Props> = ({ type, children, className, ...props }) => {
     <button
       type={type ?? "button"}
       className={clsx(
-        "rounded border border-gray-200 px-4 py-2 text-xl text-black hover:bg-black hover:text-white dark:bg-white hover:dark:bg-black hover:dark:text-white",
+        "rounded border border-gray-200 px-4 py-2 text-xl text-black hover:bg-black hover:text-white dark:text-white dark:hover:bg-white dark:hover:text-black",
         className,
       )}
       {...props}
