532879aa6f721b59a336bcde555ffb3171895fa0
MalikSabirov <maaliksabirov@gmail.com>
2023-11-27 00:35:23 +0600

    add: Loader

---
+++ src/shared/ui/loader/loader.tsx
@@ -0,0 +1,7 @@
+export const Loader = () => {
+  return (
+    <div className="absolute inset-0 z-10 flex h-full w-full items-center justify-center backdrop-blur-sm">
+      <p>Загрузка...</p>
+    </div>
+  )
+}
