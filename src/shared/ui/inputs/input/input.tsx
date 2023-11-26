226bbcbea940b4d667d460ded3f72c0bb900ffc1
MalikSabirov <maaliksabirov@gmail.com>
2023-11-26 18:52:26 +0600

    add: input

---
+++ src/shared/ui/inputs/input/input.tsx
@@ -0,0 +1,32 @@
+import clsx from "clsx"
+import { Field, FieldConfig, useField } from "formik"
+import { FC, InputHTMLAttributes } from "react"
+
+type Props = FieldConfig<string> &
+  InputHTMLAttributes<HTMLInputElement> & {
+    containerClassName?: string
+  }
+
+export const Input: FC<Props> = ({
+  containerClassName,
+  className,
+  ...props
+}) => {
+  const [field, meta] = useField(props)
+
+  return (
+    <div className={clsx("relative", containerClassName)}>
+      {meta.touched && meta.error && (
+        <label className="absolute -top-5 right-0">{meta.error}</label>
+      )}
+      <Field
+        className={clsx(
+          "w-full rounded-md border border-gray-200 p-3 dark:bg-black dark:text-white",
+          className,
+        )}
+        {...props}
+        {...field}
+      />
+    </div>
+  )
+}
