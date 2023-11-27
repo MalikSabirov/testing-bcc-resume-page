3bd79a8a8c75d85ae40ce39910e0960326d0ece4
MalikSabirov <maaliksabirov@gmail.com>
2023-11-28 00:42:12 +0600

    add: ContentWrapper

--- src/modules/home/home.tsx
+++ src/modules/home/home.tsx
@@ -1,9 +1,10 @@
+import { ContentWrapper } from "@modules/layouts/content-wrapper/content-wrapper"
 import { IntroSection } from "./components"
 
 export const Home = () => {
   return (
-    <div>
+    <ContentWrapper>
       <IntroSection />
-    </div>
+    </ContentWrapper>
   )
 }
---
+++ src/modules/layouts/content-wrapper/content-wrapper.tsx
@@ -0,0 +1,16 @@
+import { WithChildren } from "@shared/types/general"
+import clsx from "clsx"
+import { FC } from "react"
+
+type Props = WithChildren & {
+  className?: string
+}
+
+export const ContentWrapper: FC<Props> = ({ className, children }) => {
+  return (
+    <div
+      className={clsx("mx-auto w-full max-w-6xl px-8", className)}
+      children={children}
+    />
+  )
+}
