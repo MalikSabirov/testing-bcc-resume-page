1e718bb1876e7bb6f060f38039e993b9e08641d6
MalikSabirov <s.malik@mybuh.kz>
2023-11-27 10:28:50 +0600

    add: styled link component

---
+++ src/shared/ui/styled-link/styled-link.tsx
@@ -0,0 +1,16 @@
+import clsx from "clsx"
+import { FC } from "react"
+import { NavLink, NavLinkProps } from "react-router-dom"
+
+type Props = NavLinkProps
+
+export const StyledLink: FC<Props> = ({ className, ...props }) => {
+  return (
+    <NavLink
+      className={clsx(
+        "rounded border border-gray-200 px-4 py-2 text-xl text-black hover:bg-black hover:text-white dark:text-white dark:hover:bg-white dark:hover:text-black",
+      )}
+      {...props}
+    />
+  )
+}
